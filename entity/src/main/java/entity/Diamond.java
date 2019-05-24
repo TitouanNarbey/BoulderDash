993c61bce7368c7bcf0e5cdd04b70a2f7d9dc51d
Titouan Narbey <titouan.narbey@gmail.com>
2019-05-24 10:52:15 +0200

    Add des fils pr les dernier commit

---
+++ entity/src/main/java/entity/Diamond.java
@@ -0,0 +1,21 @@
+package entity;
+
+import contract.IGravity;
+import contract.ISliding;
+import contract.IDestroyable;
+
+public class Diamond extends Block implements IGravity, ISliding, IDestroyable{
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void fall() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
