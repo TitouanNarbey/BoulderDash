d2e82ffff8e1f56a215602df528f198a0a1f76f3
Gled07 <gledgjyriqi@gmail.com>
2019-05-24 10:05:02 +0200

    MAP de test

--- entity/src/main/java/entity/Entity.java
+++ entity/src/main/java/entity/Entity.java
@@ -16,8 +16,39 @@ Air = espace
 Diamond = D
 Border = B
 Exit = E
-Dirt = D
+Dirt = P
 Player = P
 Monster = M
+SpawnPoint = _
+*/
+
+/*
+Pour la MAP:
+On aura 20 lignes en verticale et 20 en horizontale 
+Voici un exemple de la MAP sans avoir inseré les objets dedans.
+BBBBBBBBBBBBBBBBBBBB
+BPPPPPPPPPP RD R PPB
+BPP_PPPPPPP  RR  PPB
+B                  B
+B                  B
+B        RR        B
+BRRR   RRDDR       B
+BDD                B
+B             RR   B
+B    RR       D    B
+B    D             B
+B        RR        B
+B        RD        B
+B                  B
+B             RD   B
+B  RR         RR   B
+B   D              B
+B        RR      E B
+B        D         B
+BBBBBBBBBBBBBBBBBBBB
+:20x20
+:11 D
+Il faudra donc inserer tout les objets ainsi que le joueur et les monstres. 
+
 
 */
